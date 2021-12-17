2bd53bd043e734793cd8c6732d485360bd3765f1
mathew-at-kiira <mathew@kiira.io>
2021-12-17 09:45:54 -0500

    implemented error state for providers screen

--- src/screens/patient/dashboard/book/screens/selectProvider/index.tsx
+++ src/screens/patient/dashboard/book/screens/selectProvider/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { TouchableOpacity, FlatList, ScrollView, Image } from 'react-native';
+import { TouchableOpacity, FlatList, ScrollView, Image, StyleSheet, Modal } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import { useSelector, useDispatch } from 'react-redux';
 import { setAppointmentExpert } from '~/redux/reducers/appointments';
@@ -11,8 +11,9 @@ import {
 } from '~/utils/functions/handleNavigation';
 import { select_provider } from '~/components/styles';
 import { h1, h2, h3, default as globalStyles } from '~/components/styles';
-import Constant, { colors, icons } from '~/utils/constants';
-const { staticImages } = Constant.App;
+import { icons,colors, images } from '~/utils/constants';
+import { positional } from 'yargs';
+
 
 const {
     medium,
@@ -45,6 +46,7 @@ const SelectProvider = () => {
     const dispatch = useDispatch();
     const [availableExperts, setAvailableExperts] = useState([]);
     const [showModal, setShowModal] = useState(false);
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
     const [expert, setExpert] = useState(null);
     const experts = useSelector((state: RootState) => state.experts.data);
     const { visit } = useSelector((state: RootState) => state.appointments);
@@ -83,6 +85,11 @@ const SelectProvider = () => {
     const toggleModal = () => {
         setShowModal(!showModal);
     };
+
+    const toggleUpdateModal = () => {
+        setShowUpdateModal(!showUpdateModal);
+    };
+
 
     const handleSelection = expert => {
         const { uid, calendarID, profileInfo, expertName } = expert;
@@ -128,6 +135,59 @@ const SelectProvider = () => {
         );
     };
 
+    const ErrorModal = () =>(
+        <Kiira.Modal 
+        styles={{
+            
+        }}
+            visible={showUpdateModal}
+            onBackdropPress={toggleUpdateModal}
+        >
+            <Kiira.Column options={[white_bg]}>
+        <Kiira.Text options={[h1]}>Thank you for reaching out!</Kiira.Text>
+            <Kiira.Text options={[h3, sm_pad_v]}>
+                We want to ensure that you are able to get the care you need. Please check your email for an update. You should receive a message shortly.
+            </Kiira.Text>
+           
+        </Kiira.Column>
+        <Kiira.Column options={[styles.buttonContainer]}>
+            <Kiira.Button
+                onPress={toggleUpdateModal}
+                title="Go to home"
+                style={{ container: [sm_pad_v, pad_h], title: [] }}
+            />
+                 </Kiira.Column>
+        </Kiira.Modal>
+    )
+const ErrorState = () => (
+    <Kiira.Screen test="Book Screen">
+         <Kiira.Header title="Select Provider" onBack={handleBack} />
+        <Kiira.Column options={[align_items_c, justify_c]}>
+        <Image
+                    style={styles.logo}
+                    resizeMode="contain"
+                    source={images.kiiraLogo}
+                />
+            
+        </Kiira.Column>
+        <Kiira.Column options={[white_bg,{flex:2}]}>
+        <Kiira.Text options={[h1]}>Uh oh! It looks like there aren't any providers available.</Kiira.Text>
+           
+            <Kiira.Text options={[h3, sm_pad_v]}>
+                Due to licensing, video visits are currently limited to covered locations.Please let us know if you would like an update on availability in your area.
+            </Kiira.Text>
+           
+        </Kiira.Column>
+        <Kiira.Column options={[styles.buttonContainer]}>
+            <Kiira.Button
+                onPress={toggleUpdateModal}
+                title="Request Update"
+                style={{ container: [sm_pad_v, pad_h], title: [] }}
+            />
+                 </Kiira.Column>
+                 {showUpdateModal && <ErrorModal />}
+                 </Kiira.Screen>
+)
     const ExpertModal = () => (
         <Kiira.Modal
             styles={{
@@ -205,57 +265,40 @@ const SelectProvider = () => {
     );
     console.log('----AVAILABLE EXPERTS-----',availableExperts)
     return (
+        availableExperts.length !== 0 ?    
         <Kiira.Screen test="Book Screen">
-            <Kiira.Header title="Book Visit" onBack={handleBack} />
-            <Kiira.Heading options={[text_align_c]}>
+            <Kiira.Header title="Select Provider" onBack={handleBack} />
+           <Kiira.Heading options={[text_align_c]}>
                 Please choose a provider
-            </Kiira.Heading>
+            </Kiira.Heading>   
             <FlatList
                 data={availableExperts}
                 keyExtractor={item => item.expertName}
                 renderItem={({ item }) => {
                     return <ExpertDetails expert={item} />;
                 }}
-            />
-       {availableExperts.length === 0 && 
-       <Kiira.Screen><Kiira.Column options={[align_items_c, justify_c]}>
-                <Image
-                    resizeMode="contain"
-                    style={[height_50, width_50]}
-                    source={staticImages.penguin}
-                />
-                
-            </Kiira.Column></Kiira.Screen>}
-            
-            <Kiira.Column options={[white_bg]}>
-            <Kiira.Text options={[h1]}>Uh oh! It looks like there aren't any providers available.</Kiira.Text>
-                {/* <Text options={[h2]}>{time}</Text>
-                <Text options={[h3, sm_pad_v]}>
-                    Please follow the appointments button in the dashboard 5
-                    minutes before your visit in order to start your
-                    appointment.
-                </Text>
-                <Text options={[h3, sm_pad_v]}>
-                    Should you need to cancel, please do so at least 24 hours in
-                    advance.
-                </Text>
-                <Text options={[h3, sm_pad_v]}>
-                    Please fill out your health intake form prior to the
-                    session.
-                </Text> */}
-            </Kiira.Column>
-            {/* <Kiira.Column options={[styles.buttonContainer]}>
-                <Kiira.Button
-                    onPress={() => navigation.navigate(screenNames.Home)}
-                    title="Go Home"
-                    style={{
-                        container: styles.homeButton,
-                        title: styles.homeButtonTitle,
-                    }}
-                /> */}
-            {showModal && <ExpertModal />}
-        </Kiira.Screen>
+            />   
+            {showModal && <ExpertModal /> }  
+           
+        </Kiira.Screen> : <ErrorState />
     );
 };
 
 export default SelectProvider;
+const styles = StyleSheet.create({
+    buttonContainer: {
+        padding: 0,
+        margin: 0,
+        flex: 0,
+        backgroundColor: colors.white,
+
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        alignSelf: 'center',
+        justifyContent:'center',
+
+    },
+
+});
