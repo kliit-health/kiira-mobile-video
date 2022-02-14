ab865feec9c0130c01699abbe6de79b6221e382e
mathew-at-kiira <mathew@kiira.io>
2022-02-14 14:19:01 -0500

    Fixed schedule appointment buuton padding

--- src/screens/patient/dashboard/appointments/components/none.tsx
+++ src/screens/patient/dashboard/appointments/components/none.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '~/redux/reducers';
 import { Column, TextButton } from '~/components';
-import { Image, Text } from 'react-native';
+import { Image, Platform, Text } from 'react-native';
 import { handleNavigation } from '~/utils/functions';
 import { images, screenNames } from '~/utils/constants';
 import styles from '../style';
@@ -22,7 +22,7 @@ export const None = () => {
             />
             <Text style={styles.title}>{lang.appointments.noVisits}</Text>
             <TextButton
-                styles={{ root: { marginTop: 'auto' } }}
+                styles={Platform .OS == 'android' ? { root: { marginTop: 'auto',marginBottom:'3%' } }: { root: { marginTop: 'auto'} }}
                 onPress={() => handleNavigation(screenNames.Book)}
             >
                 {lang.appointments.scheduleAppointment}
