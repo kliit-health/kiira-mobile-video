031363605b542f1309417f0f1506a343027ba153
Jackson <starhelios94@gmail.com>
2022-03-22 14:57:31 -0400

    changed env with kiira-health-dev

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -208,7 +208,7 @@ export async function getAppointmentsByDayAsync(data) {
  
     try {
         const times = {};
-        await fetch(urls.prod.appointmentGetByDay, obj)
+        await fetch(urls.staging.appointmentGetByDay, obj)
             .then(res => res.json())
             .then(data => (times.current = data));
         return times;
@@ -242,7 +242,7 @@ export async function getAppointmentDatesAsync(data) {
         };
 
         let response = [];
-        await fetch(urls.prod.appointmentGetByMonth, obj)
+        await fetch(urls.staging.appointmentGetByMonth, obj)
             .then(res => res.json())
             .then(data => {
                 response = [...response, ...data];
@@ -263,7 +263,7 @@ export async function getAppointmentDatesAsync(data) {
             }),
         };
 
-        await fetch(urls.prod.appointmentGetByMonth, obj)
+        await fetch(urls.staging.appointmentGetByMonth, obj)
             .then(res => res.json())
             .then(data => {
                 response = [...response, ...data];
@@ -324,7 +324,7 @@ export async function makeAppointment(data) {
         };
 
         let response;
-        let checkTime = await fetch(urls.prod.appointmentCheckTime, obj)
+        let checkTime = await fetch(urls.staging.appointmentCheckTime, obj)
             .then(res => res.json())
             .then(data => data)
             .catch(error => {
@@ -332,7 +332,7 @@ export async function makeAppointment(data) {
             });
 
         if (checkTime.valid) {
-            await fetch(urls.prod.appointmentMake, obj)
+            await fetch(urls.staging.appointmentMake, obj)
                 .then(res => res.json())
                 .then(res => {
                     response = {
@@ -485,7 +485,7 @@ export async function cancelAppointmentAsync(data) {
     };
 
     try {
-        return await fetch(urls.prod.appointmentCancel, obj)
+        return await fetch(urls.staging.appointmentCancel, obj)
             .then(res => {
                 let response = res.json();
                 return response;
@@ -552,7 +552,7 @@ export async function changeAppointmentAsync(data) {
     };
 
     try {
-        return await fetch(urls.prod.appointmentChange, obj)
+        return await fetch(urls.staging.appointmentChange, obj)
             .then(res => res.json())
             .then(async res => {
                 if (res.body.error) {
