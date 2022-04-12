efac2266742cf759047da47c3fb3a25ffc2ab7ef
mathew-at-kiira <mathew@kiira.io>
2022-04-12 23:33:07 +0530

    add snapshot test for payment screen

--- src/__tests__/app.test.tsx
+++ src/__tests__/app.test.tsx
@@ -6,6 +6,7 @@ import renderer from 'react-test-renderer';
 import React, { useRef } from 'react'; 
 import Appointments from '../screens/provider/appointments';  
 import ExpertTwillioLogin from '../screens/provider/appointments/twillio/Login';
+import Payment from '../screens/patient/dashboard/book/screens/payment';
 import ExpertTwillioCalling from '../screens/provider/appointments/twillio/Callling';
 import { TwilioVideo } from 'react-native-twilio-video-webrtc';
 
@@ -22,5 +23,10 @@ describe('Testing for reason type in appointment #195', () => {
         expect(expertTwillioLoginTree.toJSON()).toMatchSnapshot();
     }); 
    
+    const payment = renderer.create(<Payment />);
+    test('payment', () => {
+        expect(payment).toBeDefined();
+        expect(payment.toJSON()).toMatchSnapshot();
+    });
 });  
  
--- src/__tests__/jestInit.js
+++ src/__tests__/jestInit.js
@@ -1,174 +1,218 @@
-import React, {Component} from 'react';
-jest.mock('@notifee/react-native', () => 'notifee')
-jest.mock('react-native-device-info', () => require('react-native-device-info/mock'));
-jest.mock('react-native-permissions', () => require('react-native-permissions/mock'));
+import React, { Component } from 'react';
+import mockStripe from '@stripe/stripe-react-native/jest/mock.js';
+
+jest.mock('@notifee/react-native', () => 'notifee');
+jest.mock('react-native-device-info', () =>
+    require('react-native-device-info/mock'),
+);
+jest.mock('react-native-permissions', () =>
+    require('react-native-permissions/mock'),
+);
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
-jest.mock('../../node_modules/@react-native-firebase/app/lib/internal/RNFBNativeEventEmitter.js', () => { 
-    const { EventEmitter } = require('events'); 
-    return EventEmitter; 
-}); 
+jest.mock(
+    '../../node_modules/@react-native-firebase/app/lib/internal/RNFBNativeEventEmitter.js',
+    () => {
+        const { EventEmitter } = require('events');
+        return EventEmitter;
+    },
+);
 jest.mock('react-native-device-info', () => {
-return {
-    getVersion: () => 4
-}
-})
-jest.mock('@stripe/stripe-react-native', () => ({
-    StripeProvider: jest.fn(({children}) => children),
-    CardField: jest.fn(() => null),
-    presentPaymentSheet: jest.fn(),
-    initPaymentSheet: jest.fn(),
-}));
-jest.mock('react-native-text-input-mask', () => 'TextInputMask')
+    return {
+        getVersion: () => 4,
+    };
+});
+jest.mock('@stripe/stripe-react-native', () => mockStripe);
+jest.mock('react-native-text-input-mask', () => 'TextInputMask');
 
 jest.mock('react-native-safe-area-context', () => ({
-    useSafeAreaInsets: jest.fn(() => (
-        {
-            left: 0,
-            right: 0,
-            bottom: 0,
-            top: 0,
-        }
-    )),
+    useSafeAreaInsets: jest.fn(() => ({
+        left: 0,
+        right: 0,
+        bottom: 0,
+        top: 0,
+    })),
 }));
 
-jest.mock('../svgs/search.svg', () => 'Search')
+jest.mock('../svgs/search.svg', () => 'Search');
 
-jest.mock('react-redux', () => 'useDispatch')
- 
+jest.mock('react-redux', () => 'useDispatch');
 
 const mockDispatch = jest.fn();
 jest.mock('react-redux', () => {
     return {
-      connect: (mapStateToProps, mapDispatchToProps) => (ReactComponent) => ({
-        mapStateToProps,
-        mapDispatchToProps,
-        ReactComponent
-      }),
-      Provider: ({ children }) => children,
-      useDispatch: () => mockDispatch,
-      useSelector: () => ({
-        navigator:{
-            previousRoute: 'Confirm',
-        },
-        expertAppointments: {
-            title: "Test Appointment"
-        },
-        visitSummary: {
-            afterVisitSummary: 'afterVisitSummary',
-            visitInformation: 'visitInformation',
-            chiefComplaint: 'chiefComplaint',
-            assessmentPlan: 'assessmentPlan',
-            patientName: 'patientName',
-            dateAndTime: 'dateAndTime',
-            provider: 'provider',
-            locked: 'locked',
-            title: 'title',
-        },
-        visit: {
-            expert: {
-                uid: 'uid'
-            }, 
-            uid: 'uid'
-        }
-      }),
-    }
-  })  
+        connect: (mapStateToProps, mapDispatchToProps) => ReactComponent => ({
+            mapStateToProps,
+            mapDispatchToProps,
+            ReactComponent,
+        }),
+        Provider: ({ children }) => children,
+        useDispatch: () => mockDispatch,
+        useSelector: () => ({
+            navigator: {
+                previousRoute: 'Confirm',
+            },
+            expertAppointments: {
+                title: 'Test Appointment',
+            },
+            visitSummary: {
+                afterVisitSummary: 'afterVisitSummary',
+                visitInformation: 'visitInformation',
+                chiefComplaint: 'chiefComplaint',
+                assessmentPlan: 'assessmentPlan',
+                patientName: 'patientName',
+                dateAndTime: 'dateAndTime',
+                provider: 'provider',
+                locked: 'locked',
+                title: 'title',
+            },
+            profileInfo: {
+                firstName: 'firstName',
+                lastName: 'lastName',
+                profileImageUrl: 'profileImageUrl',
+                pronouns: 'pronouns',
+                phoneNumber: 'phoneNumber',
+                dob: 'dob',
+                gender: 'gender',
+                insurance: 'insurance',
+            },
+            visit: {
+                expert: {
+                    uid: 'uid',
+                    profileInfo: {
+                        firstName: 'firstName',
+                        lastName: 'lastName',
+                        profession: 'profession',
+                        imageUrl: 'imageUrl',
+                        phoneNumber: 'phoneNumber',
+                        rating: 'rating',
+                    },
+                },
+                uid: 'uid',
+                time: {
+                    date: 'date',
+                },
+            },
+        }),
+    };
+});
 
-jest.mock("react-navigation-stack", () => {
+jest.mock('react-navigation-stack', () => {
     return {
         createStackNavigator: jest.fn().mockImplementation(nav => {
             return {
                 router: {
-                    getStateForAction: jest.fn().mockImplementation(component => component)
-                }
+                    getStateForAction: jest
+                        .fn()
+                        .mockImplementation(component => component),
+                },
             };
-        }), 
-    }
-  })
- 
-jest.mock("react-navigation", () => {
-    return {
-      createAppContainer: jest
-        .fn()
-        .mockReturnValue(function NavigationContainer(props) {
-          return null;
         }),
-      createDrawerNavigator: jest.fn().mockImplementation(nav => {
-        return {};
-      }),
-      createMaterialTopTabNavigator: jest.fn(),
-      createSwitchNavigator: jest.fn().mockImplementation(nav => {
-        return {};
-      }),
-      StackRouter: jest.fn().mockImplementation(nav => {
-        return {};
-      }),
-      createNavigator: jest.fn().mockImplementation(component => component),
-      TabRouter: jest.fn().mockImplementation(component => component),
-      createBottomTabNavigator: jest.fn(),
-      withNavigation: jest.fn().mockImplementation(navigation => {
-        return {
-            state:{
-                params:{
-
-                }
-            }
-        };
-      }),
-      StackActions: {
-        push: jest
-          .fn()
-          .mockImplementation(x => ({ ...x, type: "Navigation/PUSH" })),
-        replace: jest
-          .fn()
-          .mockImplementation(x => ({ ...x, type: "Navigation/REPLACE" }))
-      },
-      NavigationActions: {
-        navigate: jest.fn().mockImplementation(x => x)
-      },
-      ThemeColors: {
-        light: {
-          bodyContent: ""
-        },
-        dark: {
-          bodyContent: ""
-        }
-      }, 
     };
-  }); 
+});
 
-  jest.mock("react-native-twilio-video-webrtc", () => {
+jest.mock('react-navigation', () => {
     return {
-        TwilioVideo: (Component) => {
+        createAppContainer: jest
+            .fn()
+            .mockReturnValue(function NavigationContainer(props) {
+                return null;
+            }),
+        createDrawerNavigator: jest.fn().mockImplementation(nav => {
+            return {};
+        }),
+        createMaterialTopTabNavigator: jest.fn(),
+        createSwitchNavigator: jest.fn().mockImplementation(nav => {
+            return {};
+        }),
+        StackRouter: jest.fn().mockImplementation(nav => {
+            return {};
+        }),
+        createNavigator: jest.fn().mockImplementation(component => component),
+        TabRouter: jest.fn().mockImplementation(component => component),
+        createBottomTabNavigator: jest.fn(),
+        withNavigation: jest.fn().mockImplementation(navigation => {
+            return {
+                state: {
+                    params: {},
+                },
+            };
+        }),
+        StackActions: {
+            push: jest
+                .fn()
+                .mockImplementation(x => ({ ...x, type: 'Navigation/PUSH' })),
+            replace: jest.fn().mockImplementation(x => ({
+                ...x,
+                type: 'Navigation/REPLACE',
+            })),
+        },
+        NavigationActions: {
+            navigate: jest.fn().mockImplementation(x => x),
+        },
+        ThemeColors: {
+            light: {
+                bodyContent: '',
+            },
+            dark: {
+                bodyContent: '',
+            },
+        },
+    };
+});
+
+jest.mock('react-native-twilio-video-webrtc', () => {
+    return {
+        TwilioVideo: Component => {
             return [
-                Component.connect = jest.fn().mockImplementation(component => component),
-                Component.disconnect = jest.fn().mockImplementation(component => component),
-                Component.setLocalAudioEnabled = jest.fn().mockImplementation(component => component),
-                Component.setLocalVideoEnabled = jest.fn().mockImplementation(component => component),
-                Component.toggleSoundSetup = jest.fn().mockImplementation(component => component),
-                Component.flipCamera = jest.fn().mockImplementation(component => component),
-                Component.setRemoteAudioPlayback = jest.fn().mockImplementation(component => component),
-                Component.getStats = jest.fn().mockImplementation(component => component),
-                Component.onRoomDidConnect = jest.fn().mockImplementation(component => component),
-                Component.onRoomDidFailToConnect = jest.fn().mockImplementation(component => component),
-                Component.onParticipantAddedVideoTrack = jest.fn().mockImplementation(component => component),
-                Component.onParticipantRemovedVideoTrack = jest.fn().mockImplementation(component => component),
-            ]
+                (Component.connect = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.disconnect = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.setLocalAudioEnabled = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.setLocalVideoEnabled = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.toggleSoundSetup = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.flipCamera = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.setRemoteAudioPlayback = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.getStats = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.onRoomDidConnect = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.onRoomDidFailToConnect = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.onParticipantAddedVideoTrack = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+                (Component.onParticipantRemovedVideoTrack = jest
+                    .fn()
+                    .mockImplementation(component => component)),
+            ];
         },
-        TwilioVideoLocalView: (Component) => {
+        TwilioVideoLocalView: Component => {
             return [
-                Component.enabled = jest.fn().mockImplementation(x => x),
-                Component.style = jest.fn().mockImplementation(x => x)
-            ]
+                (Component.enabled = jest.fn().mockImplementation(x => x)),
+                (Component.style = jest.fn().mockImplementation(x => x)),
+            ];
         },
-        TwilioVideoParticipantView:{
+        TwilioVideoParticipantView: {},
+    };
+});
 
-        },
-    }
-  }); 
-
-  jest.mock('react-native-keep-awake', () => 'KeepAwake');
-  jest.mock("react-native-reanimated", () => require("react-native-reanimated/mock"))
-
- 
-  
+jest.mock('react-native-keep-awake', () => 'KeepAwake');
+jest.mock('react-native-reanimated', () =>
+    require('react-native-reanimated/mock'),
+);
